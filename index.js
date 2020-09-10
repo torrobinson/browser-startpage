b131bcd6dfaa49d14f8312274bb1282c55465d54
Tor <torrobinson@gmail.com>
2020-09-09 21:09:01 -0600

    Small progress

--- index.js
+++ index.js
@@ -11,14 +11,33 @@ window.addEventListener('load', function(event) {
         var now = new Date();
         clock.innerHTML = formatDate(now, "hh:mmtt");
         weekday.innerHTML = formatDate(now, "dddd");
-        date.innerHTML = formatDate(now, "MMMM d, yyyy");
+        date.innerHTML = formatDate(now, "MMM d, yyyy");
+
+        // Set greeting based on time
+        var hours = now.getHours();
+        var greetingMessage = 'Welcome';
+        if(hours >=6 && hours < 12)
+            greetingMessage = "Good Morning"
+        else if(hours >=12 && hours <16)
+            greetingMessage = "Good Afternoon"
+        else if(hours >=16 && hours <20)
+            greetingMessage = "Good Evening"
+        else if(hours >=20 || hours < 6)
+            greetingMessage = "Good Night"
+        greeting.innerHTML = greetingMessage;
+
     }, 1000);
 
     // If there's a name provided, greet them. Else, don't.
     if(queryStringParameters.has('name')){
-        name.innerHTML = "Welcome, " + queryString('name');
+        name.innerHTML = queryString('name');
     }
     else {
         greeting.parentNode.removeChild(greeting);
     }
+
+
+
+
+    feather.replace();
 });
