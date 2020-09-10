a61bd729ec50ff3f4559217b2628b9578842461d
Tor <torrobinson@gmail.com>
2020-09-10 09:16:55 -0600

    Weather working. Made all inputss into query string params

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+var currentLocation = 'New York';
+var weatherApiKey = '';
+// Set the weather api key
+if(queryStringParameters.has('weatherApiKey')){
+    weatherApiKey = queryString('weatherApiKey');
+}
+
 window.addEventListener('load', function(event) {
     // Update the clock once per second
     updateClock();
     setInterval(updateClock, 1000);
 
+    // Default the location
+    var locationElement = document.querySelector('.location');
+    if(queryStringParameters.has('location')){
+        currentLocation = queryString('location');
+    }
+    locationElement.innerHTML = currentLocation;
 
     // If there's a name provided, greet them. Else, don't.
     var name = document.querySelector('.name');
@@ -10,14 +23,17 @@ window.addEventListener('load', function(event) {
         name.innerHTML = queryString('name');
     }
     else {
-        greeting.parentNode.removeChild(greeting);
+        var greeting = document.querySelector('.greeting');
+        if(greeting){
+            var name = document.querySelector('.name');
+            greeting.parentNode.removeChild(greeting);
+            name.parentNode.removeChild(name);
+        }
     }
 
     // Update weather every 5 minutes
     updateWeather();
     setInterval(updateWeather, 300000);
-
-
 
 
     feather.replace();
@@ -35,38 +51,74 @@ function updateClock(){
 
     // Set greeting based on time
     var greeting = document.querySelector('.greeting');
-    var hours = now.getHours();
-    var greetingMessage = 'Welcome';
-    if(hours >=6 && hours < 12)
-        greetingMessage = "Good Morning"
-    else if(hours >=12 && hours <16)
-        greetingMessage = "Good Afternoon"
-    else if(hours >=16 && hours <20)
-        greetingMessage = "Good Evening"
-    else if(hours >=20 || hours < 6)
-        greetingMessage = "Good Night"
-    greeting.innerHTML = greetingMessage;
+    if(greeting){
+        var hours = now.getHours();
+        var greetingMessage = 'Welcome';
+        if(hours >=6 && hours < 12)
+            greetingMessage = "Good Morning"
+        else if(hours >=12 && hours <16)
+            greetingMessage = "Good Afternoon"
+        else if(hours >=16 && hours <20)
+            greetingMessage = "Good Evening"
+        else if(hours >=20 || hours < 6)
+            greetingMessage = "Good Night"
+        greeting.innerHTML = greetingMessage + ', ';
+    }
 }
 
 function updateWeather() {
-    var location = 'Edmonton, Alberta';
-    var weatherApiKey = 'c315bd9f957bdf78a0769079e04bdd39';
-    var api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherApiKey}`;
+    if(weatherApiKey == '') return;
+    var api = `https://api.openweathermap.org/data/2.5/weather?q=${currentLocation}&appid=${weatherApiKey}`;
     fetch(api)
     .then(function (response) {
             let data = response.json();
             return data;
         })
         .then(function (data) {
-            var celcius = Math.floor(data.main.temp - 273.15);
+            const kelvin = 273.15;
+            var celcius = Math.floor(data.main.temp - kelvin);
+            var min = Math.floor(data.main.temp_min - kelvin);
+            var max = Math.floor(data.main.temp_max - kelvin);
             var description = data.weather[0].description;
+            description = description[0].toUpperCase() + description.substring(1);
             var icon = data.weather[0].icon;
 
             var celciusElement = document.querySelector('.celcius');
+            var minElement = document.querySelector('.temp-min');
+            var maxElement = document.querySelector('.temp-max');
             var descriptionElement = document.querySelector('.weather-description');
-            var iconElement = document.querySelector('.weather-icon');
+            var iconHolderElement = document.querySelector('.weather-icon');
 
+            // Temp
             celciusElement.innerHTML =  celcius;
+            minElement.innerHTML = min + '°';
+            maxElement.innerHTML = max + '°';
+
+            // Description
             descriptionElement.innerHTML =  description;
+
+            // Icon
+            var actualIcon = '';
+            switch(icon.substring(0,2)){
+                case '01':
+                    actualIcon = 'sun';
+                case '02':
+                case '03':
+                case '04':
+                    actualIcon = 'cloud';
+                case '09':
+                case '10':
+                    actualIcon = 'cloud-rain';
+                case '11':
+                    actualIcon = 'cloud-lightning';
+                case '13':
+                    actualIcon = 'cloud-snow';
+                case '50':
+                    actualIcon = 'align-center'; // looks like mist lol
+                default:
+                    actualIcon = 'cloud';
+            }
+            iconHolderElement.innerHTML = `<i data-feather="${actualIcon}"></i>`;
+            feather.replace();
         });
 }
